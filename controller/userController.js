cc2fe11bfeb66424795b6c5cde5a305c4b160ee5
LeahBasson <leahbasson54@gmail.com>
2024-08-16 12:30:08 +0200

    updates

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import { users } from '../model/index.js'
+import { verifyAToken } from '../middleware/AuthenticateUser.js'
 
 const userRouter = express.Router()
 
@@ -10,7 +11,7 @@ userRouter.get('/', (req, res) => {
     users.fetchUsers(req, res)
 })
 
-userRouter.get ('/:id', (req, res) => {
+userRouter.get ('/:id', verifyAToken, (req, res) => {
     users.fetchUser(req, res)
 })
 
@@ -18,11 +19,11 @@ userRouter.post('/register', (req, res) => {
     users.registerUser(req, res)
 })
 
-userRouter.patch('/:id', (req, res) => {
+userRouter.patch('/:id', verifyAToken, (req, res) => {
     users.updateUser(req, res)
 })
 
-userRouter.delete('/:id', (req, res) => {
+userRouter.delete('/:id', verifyAToken, (req, res) => {
     users.deleteUser(req, res)
 })
 
