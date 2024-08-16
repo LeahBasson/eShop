5d20ff8344d536c7aab6f075d207cbbd99dd0d1d
LeahBasson <leahbasson54@gmail.com>
2024-08-16 10:08:30 +0200

    Update Index

--- controller/productController.js
+++ controller/productController.js
@@ -5,7 +5,7 @@ import { verifyAToken } from '../middleware/AuthenticateUser.js'
 
 const productRouter = express.Router()
 
-productRouter.use(bodyParser.json())
+productRouter.use(bodyParser.json()) //to not call bodyParser on each and every endpoint, register once using 'router.use' // the body parser is used to pass the body as json. when you sending data from the database you need to have a pipeline body parser is that pipeline // when the user sends a request you need to have a body parser.
 
 productRouter.get('/', verifyAToken, (req, res) => {   //a middleware which is verifyAToken is placed between an endpoint and a callback function
     products.fetchProducts(req, res) 
--- index.js
+++ index.js
@@ -1,204 +1,26 @@
-import express from 'express'
+import { userRouter, express } from './controller/userController.js'
+import { productRouter  } from './controller/productController.js'
 import path from 'path'
-import { connection as db} from './config/index.js'
-import { createToken } from './middleware/AuthenticateUser.js'
-import { hash, compare } from 'bcrypt'
-import bodyParser from 'body-parser'
+
 
 //Create an express app 
 const app = express()
 const port = +process.env.PORT || 4000
 const router = express.Router()
 
-//Middleware
-app.use(router , express.static('./static'),
+//Middleware - Middleware is software that lies between an operating system and the applications running on it. 
+app.use(router ,
+ '/user', userRouter,  //to get all the endpoints 
+ '/product' , productRouter,
+ express.static('./static'),
  express.json(), 
  express.urlencoded({
     extended:true
-})
-)
-
-router.use(bodyParser.json()), //to not call bodyParser on each and every endpoint, register once using 'router.use' // the body parser is used to pass the body as json. when you sending data from the database you need to have a pipeline body parser is that pipeline // when the user sends a request you need to have a body parser.
+}))
 
 //Endpoint // the get endpoint allows you to retrieve something
 router.get('^/$|/eShop', (req, res) => {
     res.status(200).sendFile(path.resolve('./static/html/index.html'))
-})
-
-
-router.get('/users', (req, res) => {
-    try {
-        const strQry = `SELECT firstName, lastName, age, emailAdd, UserRole, ProfileURL
-        FROM Users;`
-
-        db.query(strQry, (err, results) => {
-            if (err) throw new Error(err)
-            res.json({
-                status: res.statusCode,
-                results
-            })
-        })
-    }
-     catch(e){
-        res.json({
-            status: 404,
-            msg: e.message
-        })
-    }
-})
-
-//THIS DISPLAYS DATA ACCORDING TO ITS ID
-router.get('/users/:id', (req, res) => {
-    try{
-        const stryQry = `
-        SELECT userID, firstName, lastName, age, emailAdd, UserRole, ProfileURL
-        FROM Users WHERE userID = ${req.params.id};`
-        db.query(stryQry, (err, result) => {
-            if(err) throw new Error('Issue when retrieving a user.')
-                res.json({
-               status: res.statusCode,
-               result: result[0]
-            })
-        })
-    } catch (e) {
-        res.json({
-            status: 404,
-            msg:e.message
-        })
-    }
-})
-
-//post is more secure than get
-router.post('/register', async (req, res) => {
-    try {
-        let data = req.body
-       
-        data.pwd = await hash(data.pwd, 12 )  //if the salt is bigger than 15 characters it will take long to encrypt & decrypt
-        //Payload
-        let user = {
-            emailAdd: data.emailAdd,
-            pwd: data.pwd
-        }
-        let strQry = `
-        INSERT INTO Users
-        SET ? ;   
-        `  // or you can use this VALUES (?, ? , ?, ?)
-        db.query(strQry, [data], (err) =>{
-            if(err) {
-                res.json({
-                    status: res.statusCode,
-                    msg: 'This email has already been taken'
-                })
-            } else{
-                const token = createToken(user)
-                res.json({
-                    token,
-                    msg: 'You are now registered'
-                })
-            }
-        })
-    } catch(e) {
-        res.json({
-            status: 400, // Mistake on the clients side (Maybe syntax error)
-            msg: e.message //The error message from the if statement
-    })
-    }
-    }
-)
-
-//put - everytime you send a request to update you send a new request. Thats why you use patch so that you can work with the same request when updating.
-router.patch('/user/:id', async (req, res) => {
-    try {
-        let data = req.body
-        if (data.pwd) {
-            data.pwd = await hash (data.pwd, 12)
-        }
-        const strQry = `
-        UPDATE Users
-        SET ?
-        WHERE userID = ${req.params.id}
-        `  
-        db.query (strQry, [data], (err) => {
-            if (err) throw new Error ('Unable to update a user')
-                res.json({
-                    status: res.statusCode,
-                    msg: 'The user record was updated.'
-            })
-        })
-    } catch(e) {
-        res.json({
-            status: 400,
-            msg: e.message //The error message from the if statement
-    })
-    }
-})
-
-router.delete('/user/:id', (req, res) => {
-    try{
-        const strQry = `
-        DELETE FROM Users
-        WHERE userID = ${req.params.id};
-        `
-        db.query (strQry, (err) => {
-            if(err) throw new Error('To delete a user, please review your delete query.')
-                res.json({
-                    status: res.statusCode,
-                    msg: 'A user\'s information was removed'
-            })
-        })
-    } catch(e) {
-        res.json({
-            status: 404, //Resource not found
-            msg: e.message
-        })
-    }
-})
-
-router.post('/login', (req, res) => {
-    try{
-        const { emailAdd, pwd } = req.body
-        const strQry = `
-        SELECT userID, firstName, lastName, age,
-        emailAdd, pwd, UserRole, ProfileURL
-        FROM Users
-        WHERE emailAdd = '${emailAdd}';
-        `
-        db.query(strQry, async (err, result) => {
-            if(err) throw new Error ('To login, please review your query.')
-            if (!result?.length){
-                res.json(
-                {
-                    status: 401, // Unauthorized user
-                    msg: 'You provided a wrong email.'
-                }
-            )
-            } else{
-                const isValidPass = await compare
-                (pwd, result[0].pwd)
-                if (isValidPass) {
-                    const token = createToken({
-                        emailAdd, 
-                        pwd
-                    })
-                    res.json({
-                        status: res.statusCode,
-                        token,
-                        result: result[0]
-                    })
-                } else {
-                    res.json({
-                        status: 401,
-                        msg: 'Invalid password or you have not registered'
-                    })
-                }
-            }
-        } )
-    } catch (e) {
-        res.json({
-            status: 404,
-            msg: e.message
-        })
-    }
 })
 
 //If you looking for something that doesnt exist
