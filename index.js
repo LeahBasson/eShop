4558442f78726397206d150d0c3117696c5564ba
LeahBasson <leahbasson54@gmail.com>
2024-08-19 14:20:57 +0200

    updates

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const port = +process.env.PORT || 4000
 
 //Middleware - Middleware is software that lies between an operating system and the applications running on it. 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); //enables your server to accept requests from any origin.
+    res.header("Access-Control-Allow-Origin", "*");  //enables your server to accept requests from any origin.
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
     next() //The next() function is called to pass control to the next middleware function in the stack.
 })
 app.use('/user', userRouter) //to get all the endpoints  //Brackets to parse a string
